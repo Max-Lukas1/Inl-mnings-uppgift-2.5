7576f966663effc23c71df798475ab689b266d39
majo06002 <Max-Lukas@outlook.com>
2023-09-20 10:50:53 +0200

    Add project files.

---
+++ Inlämnings uppgift 2.5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace inlämningsuppgift_2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett decimaltal");
+            string Decimaltal1=Console.ReadLine();
+            Double Decimaltals1=Double.Parse(Decimaltal1);
+
+            Console.WriteLine("Skriv ett till decimaltal");
+            string Decimaltal2 = Console.ReadLine();
+            Double Decimaltals2 = Double.Parse(Decimaltal1);
+
+            Double svar=Decimaltals1*Decimaltals2;
+            Console.WriteLine(svar);
+        }
+    }
+}
+
